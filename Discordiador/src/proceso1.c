4d562bf8a1ec45e4b92929828e87263af2c9f84c
Gaspar Habif <gasparhabif@gmail.com>
2021-04-28 19:50:53 -0300

    Beutify example

--- Discordiador/src/proceso1.c
+++ Discordiador/src/proceso1.c
@@ -3,6 +3,6 @@
 int main()
 {
     t_cpu_conf *config = get_cpu_config("../Discordiador/cfg/config.cfg");
-    printf("En el proceso 1 llega: %i", config->ip_ram);
+    printf("En el proceso 1 llega: %s\n", config->ip_ram);
     free(config);
 }
