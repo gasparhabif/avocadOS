4b83d074139acb9a204f97395ab0d6cee97fe087
Gaspar Habif <gasparhabif@gmail.com>
2021-04-28 19:19:12 -0300

    Updated example

--- Discordiador/src/proceso1.c
+++ Discordiador/src/proceso1.c
@@ -3,7 +3,7 @@
 int main()
 {
     t_cpu_conf *config = get_cpu_config("../cfg/config.cfg");
-    printf("Soy el proceso 1! %s", &config->puerto_mongo);
+    printf("Soy el proceso 1! %s", config->puerto_mongo);
     // printf("Soy el proceso 1! %s", &config.ip_mongo);
     free(config);
 }
