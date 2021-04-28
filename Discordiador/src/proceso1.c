0733ba3311a6400fcc54543f9bdd5b16376cbda3
Gaspar Habif <gasparhabif@gmail.com>
2021-04-28 19:48:38 -0300

    Fixed printf in proceso1

--- Discordiador/src/proceso1.c
+++ Discordiador/src/proceso1.c
@@ -3,7 +3,7 @@
 int main()
 {
     t_cpu_conf *config = get_cpu_config("../Discordiador/cfg/config.cfg");
-    printf("En el proceso 1 llega: %i", response->puerto_mongo);
+    printf("En el proceso 1 llega: %i", config->puerto_mongo);
     // printf("Soy el proceso 1! %s", &config.ip_mongo);
     free(config);
 }
