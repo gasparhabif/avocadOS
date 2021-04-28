e06395ed367df779e836c33602604d69a13283f8
Gaspar Habif <gasparhabif@gmail.com>
2021-04-28 19:50:06 -0300

    Fixed configurator and modified example

--- Discordiador/src/proceso1.c
+++ Discordiador/src/proceso1.c
@@ -3,7 +3,6 @@
 int main()
 {
     t_cpu_conf *config = get_cpu_config("../Discordiador/cfg/config.cfg");
-    printf("En el proceso 1 llega: %i", config->puerto_mongo);
-    // printf("Soy el proceso 1! %s", &config.ip_mongo);
+    printf("En el proceso 1 llega: %i", config->ip_ram);
     free(config);
 }
--- shared/src/configurator.c
+++ shared/src/configurator.c
@@ -5,21 +5,18 @@ t_cpu_conf *get_cpu_config(char *path)
     t_cpu_conf *response = malloc(sizeof(t_cpu_conf));
     t_config *basicConfig = config_create(path);
 
-    printf("Read file %s", path);
     // TODO: Add check existance: bool config_has_property(t_config*, char* key);
     // TODO: Add checks to formats
-    // (response)->ip_ram = config_get_string_value(basicConfig, "IP_MI_RAM_HQ");
-    // response->puerto_ram = config_get_int_value(basicConfig, "PUERTO_MI_RAM_HQ");
-    // (response)->ip_mongo = config_get_string_value(basicConfig, "IP_I_MONGO_STORE");
-    printf("Leo del archivo: %i", config_get_int_value(basicConfig, "PUERTO_I_MONGO_STORE"));
+    response->ip_ram = config_get_string_value(basicConfig, "IP_MI_RAM_HQ");
+    response->puerto_ram = config_get_int_value(basicConfig, "PUERTO_MI_RAM_HQ");
+    response->ip_mongo = config_get_string_value(basicConfig, "IP_I_MONGO_STORE");
     response->puerto_mongo = config_get_int_value(basicConfig, "PUERTO_I_MONGO_STORE");
-    printf("Guardo en el response: %i", response->puerto_mongo);
-    // response->grado_multitarea = config_get_int_value(basicConfig, "GRADO_MULTITAREA");
-    // // TODO: Chequear algoritmo valido
-    // response->algoritmo = config_get_string_value(basicConfig, "ALGORITMO");
-    // response->quantum = config_get_int_value(basicConfig, "QUANTUM");
-    // response->duracion_sabotaje = config_get_int_value(basicConfig, "DURACION_SABOTAJE");
-    // response->retardo_ciclo_cpu = config_get_int_value(basicConfig, "RETARDO_CICLO_CPU");
+    response->grado_multitarea = config_get_int_value(basicConfig, "GRADO_MULTITAREA");
+    // TODO: Chequear algoritmo valido
+    response->algoritmo = config_get_string_value(basicConfig, "ALGORITMO");
+    response->quantum = config_get_int_value(basicConfig, "QUANTUM");
+    response->duracion_sabotaje = config_get_int_value(basicConfig, "DURACION_SABOTAJE");
+    response->retardo_ciclo_cpu = config_get_int_value(basicConfig, "RETARDO_CICLO_CPU");
 
     free(basicConfig);
 
