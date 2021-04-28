c6c89332e61c2347d7fe463de327f87065e67e1a
Gaspar Habif <gasparhabif@gmail.com>
2021-04-28 19:01:49 -0300

    Removed logger from example

--- Discordiador/src/proceso1.c
+++ Discordiador/src/proceso1.c
@@ -3,8 +3,6 @@
 int main()
 {
     t_cpu_conf *config = get_cpu_config("../cfg/config.cfg");
-    t_log *logger = log_create("./logs/proceso1.log", "PROCESO1", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el proceso 1! %s", &config.ip_mongo);
-    log_destroy(logger);
+    printf("Soy el proceso 1! %s", &config.ip_mongo);
     free(config);
 }
