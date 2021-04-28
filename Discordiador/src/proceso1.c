b2ae035233ee41c98ad83091b2bbb75eb7340935
Gaspar Habif <gasparhabif@gmail.com>
2021-04-28 18:59:58 -0300

    Modified example

--- Discordiador/src/proceso1.c
+++ Discordiador/src/proceso1.c
@@ -4,7 +4,7 @@ int main()
 {
     t_cpu_conf *config = get_cpu_config("../cfg/config.cfg");
     t_log *logger = log_create("./logs/proceso1.log", "PROCESO1", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el proceso 1! %s", config.ip_mongo);
+    log_info(logger, "Soy el proceso 1! %s", &config.ip_mongo);
     log_destroy(logger);
     free(config);
 }
