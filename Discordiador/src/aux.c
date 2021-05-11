b3b4391c6ffbe922e0577649c95cd001ead2d7b9
Gaspar Habif <gasparhabif@gmail.com>
2021-05-11 20:12:18 -0300

    Added segmentation at leer_tareas

--- Discordiador/src/aux.c
+++ Discordiador/src/aux.c
@@ -1,6 +1,7 @@
 #include "proceso1.h"
 
-void leer_tareas(t_tareas **tareas, FILE *fpTareas){
+void leer_tareas(t_tareas **tareas, FILE *fpTareas)
+{
 
     char *line = NULL;
     char **instruccion;
@@ -15,18 +16,21 @@ void leer_tareas(t_tareas **tareas, FILE *fpTareas){
     fseek(fpTareas, 0, SEEK_SET);
 
     //RESERVO LA MEMORIA
-    *tareas = malloc(cantLineas * sizeof(t_tareas));
+    for (int m = 0; m < cantLineas; ++m)
+        tareas[m] = malloc(sizeof(**tareas) * sizeof(*tareas));
 
     //LEO LINEA A LINEA
-    for(int i = 0; i < cantLineas; i++){
-        if((read = getline(&line, &len, fpTareas)) != -1){
-
+    for (int i = 0; i < cantLineas; i++)
+    {
+        if ((read = getline(&line, &len, fpTareas)) != -1)
+        {
             instruccion = string_split(line, " ");
 
-            while(instruccion[cantInstrucciones] != NULL)
+            while (instruccion[cantInstrucciones] != NULL)
                 cantInstrucciones++;
 
-            if(cantInstrucciones != 2){
+            if (cantInstrucciones != 2)
+            {
                 //LA INSTRUCCION ES DESCARTAR BASURA o ES UN ERROR
 
                 data = string_split(instruccion[0], ";");
@@ -36,51 +40,55 @@ void leer_tareas(t_tareas **tareas, FILE *fpTareas){
                 else
                     tareas[i]->codigoTarea = MOVER_POSICION;
 
-                while(data[cantParametros] != NULL)
+                while (data[cantParametros] != NULL)
                     cantParametros++;
 
-                if(cantParametros == 4){
-                    tareas[i]->posX          = atoi(data[1]);
-                    tareas[i]->posY          = atoi(data[2]);
+                if (cantParametros == 4)
+                {
+                    tareas[i]->posX = atoi(data[1]);
+                    tareas[i]->posY = atoi(data[2]);
                     tareas[i]->duracionTarea = atoi(data[3]);
                 }
-                else{
-                    printf("La instruccion %d no fue reconocida\n", i+1);
+                else
+                {
+                    printf("La instruccion %d no fue reconocida\n", i + 1);
                 }
             }
-            else{
-                if (strcmp(instruccion[0],      "GENERAR_OXIGENO" ) == 0) 
+            else
+            {
+                if (strcmp(instruccion[0], "GENERAR_OXIGENO") == 0)
                     tareas[i]->codigoTarea = GENERAR_OXIGENO;
                 else if (strcmp(instruccion[0], "CONSUMIR_OXIGENO") == 0)
                     tareas[i]->codigoTarea = CONSUMIR_OXIGENO;
-                else if (strcmp(instruccion[0], "GENERAR_COMIDA"  ) == 0) 
+                else if (strcmp(instruccion[0], "GENERAR_COMIDA") == 0)
                     tareas[i]->codigoTarea = GENERAR_COMIDA;
-                else if (strcmp(instruccion[0], "CONSUMIR_COMIDA" ) == 0)
+                else if (strcmp(instruccion[0], "CONSUMIR_COMIDA") == 0)
                     tareas[i]->codigoTarea = CONSUMIR_COMIDA;
-                else if (strcmp(instruccion[0], "GENERAR_BASURA"  ) == 0) 
+                else if (strcmp(instruccion[0], "GENERAR_BASURA") == 0)
                     tareas[i]->codigoTarea = GENERAR_BASURA;
                 else
                     tareas[i]->codigoTarea = MOVER_POSICION;
-                
+
                 data = string_split(instruccion[1], ";");
-                
-                while(data[cantParametros] != NULL)
+                while (data[cantParametros] != NULL)
                     cantParametros++;
 
-                if(cantParametros == 4){
-                    tareas[i]->parametro     = atoi(data[0]);
-                    tareas[i]->posX          = atoi(data[1]);
-                    tareas[i]->posY          = atoi(data[2]);
+                if (cantParametros == 4)
+                {
+                    tareas[i]->parametro = atoi(data[0]);
+                    tareas[i]->posX = atoi(data[1]);
+                    tareas[i]->posY = atoi(data[2]);
                     tareas[i]->duracionTarea = atoi(data[3]);
                 }
-                else{
-                    printf("La instruccion %d no fue reconocida\n", i+1);
+                else
+                {
+                    printf("La instruccion %d no fue reconocida\n", i + 1);
                 }
             }
-            
         }
-        else{
-            printf("La instruccion %d no fue reconocida\n", i+1);
+        else
+        {
+            printf("La instruccion %d no fue reconocida\n", i + 1);
         }
 
         //ACA HAY UN SEG FAULT, nosepq?
