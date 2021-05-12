e1013f0dc2e1eaf63618b97bffd1361e0d549e74
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-05-11 21:18:34 -0300

    Add connection_handler to store threads

--- i-Mongo-Store/src/proceso2.c
+++ i-Mongo-Store/src/proceso2.c
@@ -18,37 +18,50 @@ int main()
     printf("Servidor escuchando en puerto %d\n", config->puerto);
     listen(server_instance, 100);
 
-    // Aceptar conexión
-    struct sockaddr_in client_dir;
-    unsigned int dir_size = sizeof(socklen_t);
-    int client = accept(server_instance, (void *)&client_dir, &dir_size);
-
-    // Verificar conexión recibida
-    if (client == -1)
+    pthread_t thread_id;
+    while (1)
     {
-        perror("Algo malió sal");
-        return EXIT_FAILURE;
+        // Aceptar conexión
+        struct sockaddr_in client_dir;
+        unsigned int dir_size = sizeof(socklen_t);
+        int client = accept(server_instance, (void *)&client_dir, &dir_size);
+
+        // Verificar conexión recibida
+        if (client == -1)
+        {
+            perror("Error al aceptar conexión");
+            break;
+        }
+
+        // Crear hilo
+        pthread_create(&thread_id, NULL, connection_handler, (void *)&client);
     }
 
-    printf("Recibí una conexión en el socket %d\n", client);
-    char *buffer = malloc(100);
+    free(config);
 
+    return EXIT_SUCCESS;
+}
+
+void *connection_handler(void *arg)
+{
+    int client = *(int *)arg;
+
+    printf("Recibí una conexión en el socket %d\n", client);
+
+    char *buffer = malloc(100);
     while (1)
     {
         int msg_size = recv(client, buffer, 100, 0);
 
         if (msg_size <= 0)
         {
-            perror("Algo malió sal");
+            printf("El cliente del socket %d se desconectó\n", client);
             return EXIT_FAILURE;
         }
 
         buffer[msg_size] = '\0';
-        printf("%d bytes > %s", msg_size, buffer);
+        printf("Socket %d > %s\n", client, buffer);
     }
 
     free(buffer);
-    free(config);
-
-    return EXIT_SUCCESS;
 }
